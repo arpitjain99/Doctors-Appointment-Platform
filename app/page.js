17d9e25e3f8e54d0cd093b807517a5f51acfb207
arpitjain99 <aj2305988@gmail.com>
2025-09-02 19:02:58 +0530

    letsdo

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-export default function Home() {
-  return <div className="py-12">
-    Made by Arpit Jain
-    <br />
-    <Button> Click me</Button>
-  </div>
-}
